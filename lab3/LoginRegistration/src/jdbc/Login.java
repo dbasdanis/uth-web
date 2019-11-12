089199c2e52383b677259415a8034aed7c28b1fc
dbasdanis <dbasdanis97@gmail.com>
2019-11-12 14:49:50 +0200

    Update Login.java

--- lab3/LoginRegistration/src/jdbc/Login.java
+++ lab3/LoginRegistration/src/jdbc/Login.java
@@ -1,18 +1,15 @@
 package jdbc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.sql.*;
 
 /**
  * Servlet implementation class Login
@@ -20,58 +17,55 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Login() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-		
-        String name = request.getParameter("name");
-		String password = request.getParameter("password");
-		String dbName = null;
-		String dbPassword = null;
-		
-		try {
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_register?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "dionisis1997");
-			
-			PreparedStatement ps = con.prepareStatement("select * from register where name=? and password=?");
-			ps.setString(1,name);
-			ps.setString(2,password);
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				dbName = rs.getString("name");
-				dbPassword = rs.getString("password");
-			}
-			if(name.equals(dbName)&&password.equals(dbPassword)) {
-				out.println("You have successfully logged in");
-			}
-			else {
-				response.sendRedirect("login.jsp");
-			}
-			
-			
-		} 
-		catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	
+        String name = request.getParameter("username");
+        String password = request.getParameter("password");
+        String dbName = null;
+        String dbPassword = null;
+        
+        try {
+        
+            // loading drivers for mysql
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            //creating connection with the database 
+            Connection con = DriverManager.getConnection
+                        ("jdbc:mysql://localhost:3306/login?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","dionisis1997");
+            
+            PreparedStatement ps = con.prepareStatement("select * from accounts where name=? and password=?");
+            
+            ps.setString(1, name);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+                                    
+            while(rs.next()) {
+            	dbName = rs.getString("name");
+            	dbPassword = rs.getString("password");
+            }
+            
+            
+            if(name.equals(dbName)&&password.equals(dbPassword)) {
+            	out.println("Welcome "+ dbName +".");
+            }
+            else {
+            	out.println("Unable to login.");
+            	RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+            	rd.include(request, response);
+            }
+        
+        }
+        catch(Exception se) {
+            se.printStackTrace();
+        }
 	}
 
 }
+
