4a1f9df0a4b7c9687decb40b634ba9fd5dfff10e
dbasdanis <dbasdanis97@gmail.com>
2019-11-12 14:50:25 +0200

    Update Registration.java

--- lab3/LoginRegistration/src/jdbc/Registration.java
+++ lab3/LoginRegistration/src/jdbc/Registration.java
@@ -1,61 +1,59 @@
 package jdbc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.sql.*;
 
 /**
- * Servlet implementation class Registration
+ * Servlet implementation class Register
  */
 @WebServlet("/Registration")
 public class Registration extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public Registration() {
         super();
     }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			PrintWriter out = response.getWriter();
-			
-			String name = request.getParameter("name");
-			String password = request.getParameter("password");
-			
-			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_register?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "dionisis1997");
-			
-			PreparedStatement ps = con.prepareStatement("insert into register(name,password) values(?,?);");
-			ps.setString(1, name);
-			ps.setString(2, password);
-			ps.executeUpdate();
-			
-			out.println("You have successfully registered");
-		} 
-		catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+	
+        String name = request.getParameter("username");
+        String password = request.getParameter("password");        
+        
+		
+        try {
+        
+            // loading drivers for mysql
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            //creating connection with the database 
+            Connection con = DriverManager.getConnection
+                        ("jdbc:mysql://localhost:3306/login?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","dionisis1997");
+            
+            PreparedStatement ps = con.prepareStatement
+                        ("insert into accounts values(?,?)");
+
+            ps.setString(1, name);
+            ps.setString(2, password);
+            int i = ps.executeUpdate();
+            
+            
+        	if(i > 0) {
+        		out.println("You are sucessfully registered.");
+            }
+        }
+        catch(Exception se) {
+            se.printStackTrace();
+        }
+       
 	}
 
 }
